17026af1c2cf00e7a1279fca03262e5924bb5806
Dice-K39 <daisukehashimoto39@gmail.com>
2021-02-07 12:43:47 -0500

    Create action finished.

--- recreational_areas_backend/app/controllers/sessions_controller.rb
+++ recreational_areas_backend/app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionController < ApplicationController
 
         if user
             session[:user_id] = user.id
+
+            render json: 
+            { 
+                status: :created, 
+                logged_in: true, 
+                user: user 
+            }
+        else
+            render json: { status: 401 }
         end
     end
 end
