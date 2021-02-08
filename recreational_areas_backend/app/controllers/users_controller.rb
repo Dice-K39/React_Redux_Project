563c2876ecfe13379dab641e9aa75508a9a75c4a
Dice-K39 <daisukehashimoto39@gmail.com>
2021-02-07 22:16:03 -0500

    Added if password and password confirmation matched in user creation.

--- recreational_areas_backend/app/controllers/users_controller.rb
+++ recreational_areas_backend/app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
 
-        if user.valid?
+        if user.valid? && params[:user][:password] == params[:user][:password_confirmation]
             user.save
             session[:user_id] = user.id
 
