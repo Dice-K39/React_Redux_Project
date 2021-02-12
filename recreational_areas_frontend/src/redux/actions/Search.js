6718350ae6c71d661ed81b5fe0cf7cecaaefa718
Dice-K39 <daisukehashimoto39@gmail.com>
2021-02-11 21:44:38 -0500

    Search results saved in store.

--- recreational_areas_frontend/src/redux/actions/Search.js
+++ recreational_areas_frontend/src/redux/actions/Search.js
@@ -4,43 +4,22 @@ export const searchQuery = (queries) =>
     
     url.search = new URLSearchParams(queries);
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => console.log(data));
-
-//     const { keyword, activity, state } = queries;
-//     const URL = `https://ridb.recreation.gov/api/v1/facilities?query=${keyword}&limit=50&offset=0&state=${state}&activity=${activity}`;
-//     const key = process.env.REACT_APP_API_KEY;
-// debugger
-//     return (dispatch =>
-//     {
-//         fetch(URL,
-//         {
-//             method: "GET",
-//             headers: 
-//             {
-//                 "access-control-allow-origin": '*',
-//                 "content-type": "application/json",
-//                 "accept": "application/json",
-//                 "apikey": key
-//             }
-//         })
-//             .then(res => res.json())
-//             .then(data => 
-//             {console.log(data)
-//                 dispatch(
-//                     {
-//                         type: "SEARCH_SUCCESS",
-//                         paylod:
-//                         {
-//                             results: data.results
-//                         }
-//                     }
-//                 )
-//             })
-//             .catch(err => console.log(err))
-//     })
-
-    
-
+    return (dispatch =>
+        {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => 
+                    {
+                        dispatch(
+                            {
+                                type: "SEARCH_SUCCESS",
+                                payload:
+                                {
+                                    results: data.RECDATA
+                                }
+                            }
+                        )
+                    })
+        }
+    )
 }
