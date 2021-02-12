23bdb791bc615ede0e9286b93aea4f8fd7782eb7
Dice-K39 <daisukehashimoto39@gmail.com>
2021-02-12 14:25:08 -0500

    Working on favorite button.

--- recreational_areas_frontend/src/components/search_result_display/SearchResultsCard.js
+++ recreational_areas_frontend/src/components/search_result_display/SearchResultsCard.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+const handleOnClick = props =>
+{
+    debugger
+}
+
 export const SearchResultsCard = (props) => 
 {
     return (
         <div className="content has-background-light" key={props.id}>
-            <h2>{props.area.FacilityName}<button className="favorite-btn button is-primary">Favorite</button></h2>
+            <h2>{props.area.FacilityName}<button className="favorite-btn button is-primary" onClick={() => handleOnClick(props.area)}>Favorite</button></h2>
 
             {/* cross-site scripting (XSS) danger */}
             <div dangerouslySetInnerHTML={{ __html: props.area.FacilityDescription}}></div>
