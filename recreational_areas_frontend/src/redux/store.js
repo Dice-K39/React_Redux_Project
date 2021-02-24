83c206a2aa1b727e243b7b66aa59b1cc491c23b8
Dice-K39 <daisukehashimoto39@gmail.com>
2021-02-24 18:47:22 -0500

    Added notes.

--- recreational_areas_frontend/src/redux/store.js
+++ recreational_areas_frontend/src/redux/store.js
@@ -1,15 +1,20 @@
 import{ createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
+/*
+    createStore - creates a Redux store that holds the complete state tree of an app.
+    applyMiddleware - applies middleware, which is the suggested method of extending Redux with custom functionality.
+    compose - able to apply several store enhancers.
+    combineReducers - helper function that turns an object whose values are different reducing functions into a single reducing function that can be passed to createStore.
+*/
+import thunk from "redux-thunk"; // middleware that allows functions to be returned rather than just actions in Redux. Allows delayed actions such as working with promises.
+import logger from "redux-logger"; // logs every action and state change in the dev console.
 
-import { reducer } from "./reducers/reducer";
-import { authReducer } from "./reducers/authReducer";
-import { searchResultReducer } from "./reducers/searchResultReducer";
-import { favoritesReducer } from "./reducers/favoritesReducer";
+// reducers
+import { authReducer } from "./reducers/authReducer"; // authentication reducer
+import { searchResultReducer } from "./reducers/searchResultReducer"; // search results reducer
+import { favoritesReducer } from "./reducers/favoritesReducer"; // favorites list reducer
 
 const allReducers = combineReducers(
     {
-        example: reducer,
         auth: authReducer,
         search: searchResultReducer,
         favorites: favoritesReducer
