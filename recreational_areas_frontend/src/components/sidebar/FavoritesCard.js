bb9726f829ef73157fad8d58ba8875020aa6f519
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-18 15:56:16 -0400

    Commented out live coding assessment from FavoritesCard.

--- recreational_areas_frontend/src/components/sidebar/FavoritesCard.js
+++ recreational_areas_frontend/src/components/sidebar/FavoritesCard.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 const FavoritesCard = (props) => 
 {
-    const [likes, setLikes] = useState(0) // [0, f(){}]
+    // const [likes, setLikes] = useState(0) // [0, f(){}]
 
     return (
         <div className="content c-card has-background-light" key={props.id} id={props.id}>
             {props.area.facility_name}<button className="delete-btn button is-small is-danger" onClick={props.delete}>X</button>
-            <button className="button is-small is-primary" onClick={() => setLikes(likes + 1)}>Like</button>
-            {likes}
+            {/* <button className="button is-small is-primary" onClick={() => setLikes(likes + 1)}>Like</button>
+            {likes} */}
         </div>
     )
 }
